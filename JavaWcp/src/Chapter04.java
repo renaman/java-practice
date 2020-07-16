fd2ffc260fde0df873f053a57f5a3eb63b65bfba
renaman <n.renaman@icloud.com>
2020-07-16 11:48:46 +0900

    java基本構文

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String word = "れな";
+		System.out.println(word);
+		
+		word = "こんにちは";
+		System.out.println(word);
+		
+		final int num = 7;
+		System.out.println(num);
+		
+	}
+}
