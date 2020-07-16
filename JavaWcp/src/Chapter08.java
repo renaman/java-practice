6db9769bdddf99aa25e49291e281fb5c0199723d
renaman <n.renaman@icloud.com>
2020-07-16 14:37:58 +0900

    条件分岐と繰り返し

---
+++ JavaWcp/src/Chapter08.java
@@ -0,0 +1,22 @@
+
+public class Chapter08 {
+	public static void main(String[] args) {
+		int num1 = 1;
+		while (num1 < 5) {
+			System.out.println(num1 * num1);
+			num1++;
+		}
+		
+		int[] array = {1, 10, 20, 30};
+		for (int num2 = 0; num2 < array.length; num2++) {
+			System.out.println(array[num2]);
+		}
+		
+		for (int num3: array) {
+			if (num3 % 2 == 0) {
+				continue;
+			}
+			System.out.println(num3);
+		}
+	}
+}
